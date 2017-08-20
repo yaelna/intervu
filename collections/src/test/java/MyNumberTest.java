172aaab197e1fa8cf219df9738fa06a02eff6248
mavigdor <moran@gigaspaces.com>
2017-08-20 12:04:13 +0300

    initial commit

---
+++ collections/src/test/java/MyNumberTest.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import org.junit.Test;
+
+
+public class MyNumberTest {
+
+    /**
+     * test that {@link Collections#sort(java.util.List) sorts a list of {@link MyNumber}
+     */
+    @org.junit.Test
+    public void sortTest() throws Exception {
+        //TODO implement
+    }
+}
