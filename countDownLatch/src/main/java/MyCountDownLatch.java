6b61f63c03bbee45c78af1e4cee7e9a966cfc450
moran <moran@gigaspaces.com>
2017-08-26 20:59:21 +0300

    add impl and second test. add todo for third test.

--- countDownLatch/src/main/java/MyCountDownLatch.java
+++ countDownLatch/src/main/java/MyCountDownLatch.java
@@ -1,32 +1,57 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * TODO implement abstract methods of CountDownLatch
  */
 public class MyCountDownLatch implements ICountDownLatch {
 
+    private final Object o = new Object();
+    private final AtomicInteger count;
+
     /** Constructs a CountDownLatch initialized with the given count. */
-    public MyCountDownLatch(int count) {
+    MyCountDownLatch(int count) {
         //TODO implement
+        this.count = new AtomicInteger(count);
     }
 
     @Override
     public void await() throws InterruptedException {
         //TODO implement
+        synchronized (o) {
+            while (count.get() > 0) {
+                o.wait();
+            }
+        }
     }
 
     @Override
     public boolean await(long timeout) throws InterruptedException {
         //TODO implement
-        return false;
+        long end = timeout + System.currentTimeMillis();
+        synchronized (o) {
+            while (count.get() > 0) {
+                long leftTime = end - System.currentTimeMillis();
+                if (leftTime <= 0) return false;
+                o.wait(leftTime);
+            }
+        }
+        return  (count.get() == 0);
     }
 
     @Override
     public void countDown() {
-        //TODO implement
+        if (count.get() > 0) {
+            int i = count.decrementAndGet();
+            if (i == 0) {
+                synchronized (o) {
+                    o.notifyAll();
+                }
+            }
+        }
     }
 
     @Override
     public int getCount() {
-        //TODO implement
-        return -1;
+        return count.get();
     }
 }
--- countDownLatch/src/test/java/CountDownLatchTest.java
+++ countDownLatch/src/test/java/CountDownLatchTest.java
@@ -1,13 +1,19 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 /**
  * CountDownLatch Test
  */
 public class CountDownLatchTest {
 
     @Test
-    public void test() throws InterruptedException {
+    public void test_single() throws InterruptedException {
         final ICountDownLatch latch = new MyCountDownLatch(1);
 
         Assert.assertEquals(1, latch.getCount());
@@ -22,5 +28,41 @@ public class CountDownLatchTest {
         t.run();
         latch.await();
         Assert.assertEquals(0, latch.getCount());
+    }
+
+    @Test
+    public void test_multi() throws  InterruptedException {
+        final int nThreads = 3;
+        final ICountDownLatch latch = new MyCountDownLatch(nThreads);
+
+        Assert.assertEquals(nThreads, latch.getCount());
+
+        ExecutorService executorService = Executors.newFixedThreadPool(nThreads);
+        List<Future> futures = new ArrayList<Future>();
+        for (int i=0; i<nThreads; i++) {
+            Future<?> future = executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    latch.countDown();
+                }
+            });
+            futures.add(future);
+        }
+
+        latch.await();
+        Assert.assertEquals(0, latch.getCount());
+
+        for (Future future : futures) {
+            Assert.assertTrue(future.isDone());
+        }
+    }
+
+    @Test
+    public void test_timeout() throws  InterruptedException {
+        //TODO implement
+        final ICountDownLatch latch = new MyCountDownLatch(1);
+
+        Assert.assertFalse(latch.await(1000));
+        Assert.assertEquals(1, latch.getCount());
     }
 }
